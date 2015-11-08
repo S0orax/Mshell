fc543df37802eabf374a7d57cd5f24ace639a8c5
S0orax <ttreplay@gmail.com>
2015-11-08 11:16:03 +0100

    sighandler + cmd

---
+++ cmd.c
@@ -0,0 +1,171 @@
+/* mshell - a job manager */
+
+#include <stdio.h>
+#include <signal.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "jobs.h"
+
+#define BOLD "\033[00;01m"
+#define NORM "\033[00;00m"
+
+void do_help() {
+  printf("available commands are:\n");
+  printf(" exit - cause the shell to exit\n");
+  printf(BOLD "\t exit\n" NORM);
+  printf(" jobs - display status of jobs in the current session\n");
+  printf(BOLD "\t jobs\n" NORM);
+  printf(" fg   - run a job identified by its pid or job id in the foreground\n");
+  printf(BOLD "\t fg " NORM "pid" BOLD "|" NORM "jobid \n");
+  printf(" bg   - run a job identified by its pid or job id in the background\n");
+  printf(BOLD "\t bg " NORM "pid" BOLD "|" NORM "jobid \n");
+  printf(" stop - stop a job identified by its pid or job id\n");
+  printf(BOLD "\t stop " NORM "pid" BOLD "|" NORM "jobid \n");
+  printf(" kill - kill a job identified by its pid or job id\n");
+  printf(BOLD "\t kill " NORM "pid" BOLD "|" NORM "jobid \n");
+  printf(" help - print this message\n");
+  printf(BOLD "\t help\n" NORM);
+  printf("\n");
+  printf("ctrl-z and ctrl-c can be used to send a SIGTSTP and a SIGINT, respectively\n\n");
+}
+
+/* treat_argv - Determine pid or jobid and return the associated job structure */
+struct job_t *treat_argv(char **argv) {
+  struct job_t *jobp = NULL;
+
+  /* Ignore command if no argument */
+  if (argv[1] == NULL) {
+    printf("%s command requires PID or %%jobid argument\n", argv[0]);
+    return NULL;
+  }
+
+  /* Parse the required PID or %JID arg */
+  if (isdigit((int) argv[1][0])) {
+    pid_t pid = atoi(argv[1]);
+    if (!(jobp = jobs_getjobpid(pid))) {
+      printf("(%d): No such process\n", (int) pid);
+      return NULL;
+    }
+  } else if (argv[1][0] == '%') {
+    int jid = atoi(&argv[1][1]);
+    if (!(jobp = jobs_getjobjid(jid))) {
+      printf("%s: No such job\n", argv[1]);
+      return NULL;
+    }
+  } else {
+    printf("%s: argument must be a PID or %%jobid\n", argv[0]);
+    return NULL;
+  }
+
+  return jobp;
+}
+
+
+/* do_bg - Execute the builtin bg command */
+void do_bg(char **argv) {
+  struct job_t *job;
+
+  job = treat_argv(argv);
+
+  if(job != NULL) {
+    kill(job->jb_pid, SIGCONT);
+    job->jb_state = BG;
+  
+    if(verbose)
+      printf("job with jid %d go bg\n", job->jb_jid);
+  }
+
+  return;
+}
+
+/* waitfg - Block until process pid is no longer the foreground process */
+void waitfg(pid_t pid) {
+  if(verbose)
+    printf("Start waiting\n");
+  
+  while(jobs_fgpid() == pid)
+    sleep(1);
+  
+  return;
+}
+
+/* do_fg - Execute the builtin fg command */
+void do_fg(char **argv) {
+  struct job_t *job;
+
+  job = treat_argv(argv);
+
+  if(job != NULL) {
+    kill(job->jb_pid, SIGCONT);
+    job->jb_state = FG;
+    
+    if(verbose) {
+      printf("job with jid %d go fg and current job fg id is %d\n", job->jb_jid, jobs_pid2jid(jobs_fgpid()));
+    }
+  }
+
+  waitfg(job->jb_pid);
+  
+  return;
+}
+
+/* do_stop - Execute the builtin stop command */
+void do_stop(char **argv) {
+  struct job_t *job;
+
+  job = treat_argv(argv);
+
+  if(job != NULL) {
+    kill(job->jb_pid, SIGSTOP);
+    job->jb_state = ST;
+
+    if(verbose)
+      printf("job with jid %d was stopped\n", job->jb_jid);
+  }
+  
+  return;
+}
+
+/* do_kill - Execute the builtin kill command */
+void do_kill(char **argv) {
+  struct job_t *job;
+
+  job = treat_argv(argv);
+
+  if(job != NULL) {
+    kill(job->jb_pid, SIGKILL);
+
+    if(verbose)
+      printf("job with jid %d was killed\n", job->jb_jid);
+  }
+
+  return;
+}
+
+/* do_exit - Execute the builtin exit command */
+void do_exit() {
+  struct job_t *job;
+
+  job = jobs_getstoppedjob();
+
+  if(job == NULL) {
+    exit(EXIT_SUCCESS);
+  } else {
+    printf("There is some stopped jobs");
+    if(verbose)
+      printf("Stopped job is : [%d]\n", job->jb_jid);
+  }
+
+  return;
+}
+
+/* do_jobs - Execute the builtin jobs command */
+void do_jobs() {
+  jobs_listjobs();
+  printf("\n");
+
+  return;
+}
---
+++ sighandlers.c
@@ -0,0 +1,99 @@
+/* mshell - a job manager */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+
+#include "jobs.h"
+#include "common.h"
+#include "sighandlers.h"
+
+/*
+ * wrapper for the sigaction function
+ */
+int sigaction_wrapper(int signum, handler_t * handler) {
+  struct sigaction sa;
+
+  sa.sa_handler = handler;
+  sa.sa_flags = SA_RESTART;
+  sigemptyset(&sa.sa_mask);
+  sigaction(signum, &sa, NULL);
+
+  return 1;
+}
+
+/*
+ * sigchld_handler - The kernel sends a SIGCHLD to the shell whenever
+ *     a child job terminates (becomes a zombie), or stops because it
+ *     received a SIGSTOP or SIGTSTP signal. The handler reaps all
+ *     available zombie children
+ */
+void sigchld_handler(int sig) {
+  pid_t pid;
+  int status;
+  
+  if (verbose)
+    printf("sigchld_handler: entering\n");
+
+  while((pid = waitpid(-1, &status, WNOHANG | WUNTRACED)) > 0) {
+    if(WIFEXITED(status) || WIFSIGNALED(status)) {
+      jobs_deletejob(pid);
+    } else if(WIFSTOPPED(status)) {
+      struct job_t *job;
+      
+      job = jobs_getjobpid(pid);
+      job->jb_state = ST;
+    }
+  }
+
+  if (verbose)
+    printf("sigchld_handler: exiting\n");
+
+  return;
+}
+
+/*
+ * sigint_handler - The kernel sends a SIGINT to the shell whenver the
+ *    user types ctrl-c at the keyboard.  Catch it and send it along
+ *    to the foreground job.
+ */
+void sigint_handler(int sig) {
+  pid_t pid;
+
+  if (verbose)
+    printf("sigint_handler: entering\n");
+
+  pid = jobs_fgpid();
+  if(pid > 0)
+    kill(pid, SIGINT);
+
+  if (verbose)
+    printf("sigint_handler: exiting\n");
+
+  return;
+}
+
+/*
+ * sigtstp_handler - The kernel sends a SIGTSTP to the shell whenever
+ *     the user types ctrl-z at the keyboard. Catch it and suspend the
+ *     foreground job by sending it a SIGTSTP.
+ */
+void sigtstp_handler(int sig) {
+  pid_t pid;
+  
+  if (verbose)
+    printf("sigtstp_handler: entering\n");
+
+  pid = jobs_fgpid();
+  if(pid > 0)
+    kill(pid, SIGTSTP);
+
+  if (verbose)
+    printf("sigtstp_handler: exiting\n");
+
+  return;
+}
